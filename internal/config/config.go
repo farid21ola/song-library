2c1531d3f904aafa84b9b4cbf33d953a39e17f35
farid21ola <farid.aliev.1999@bk.ru>
2025-01-03 15:52:00 +0300

    moved the configuration to .env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,22 +1,22 @@
 package config
 
 import (
-	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/joho/godotenv"
-	"os"
+	"github.com/caarlos0/env/v11"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Env              string `yaml:"env" env-default:"local"`
-	ConnectionString string `yaml:"connection_string" env-required:"true"`
-	HTTPServer       `yaml:"http_server"`
+	Env              string `env:"ENV" env-default:"local"`
+	ConnectionString string `env:"CONNECTION_STRING,required"`
+	HTTPServer
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8080"`
-	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
-	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	Address     string        `env:"HTTP_SERVER_ADDRESS" env-default:"localhost:8080"`
+	Timeout     time.Duration `env:"HTTP_SERVER_TIMEOUT" env-default:"4s"`
+	IdleTimeout time.Duration `env:"HTTP_SERVER_IDLE_TIMEOUT" env-default:"60s"`
 }
 
 func MustLoad() *Config {
@@ -25,32 +25,10 @@ func MustLoad() *Config {
 		panic("error loading .env file")
 	}
 
-	path := fetchConfigPath()
-	if path == "" {
-		panic("config path is empty")
-	}
-
-	return MustLoadByPath(path)
-}
-
-func MustLoadByPath(path string) *Config {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		panic("config file does not exists: " + path)
-	}
-
 	var cfg Config
-
-	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
-		panic("failed to read config: " + err.Error())
+	if err := env.Parse(&cfg); err != nil {
+		panic("failed to parse environment variables: " + err.Error())
 	}
 
 	return &cfg
-}
-
-func fetchConfigPath() string {
-	var res string
-
-	res = os.Getenv("CONFIG_PATH")
-
-	return res
 }
